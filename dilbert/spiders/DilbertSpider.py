f044a07897ee2417da2aace414580bdbae710f81
Bartlomiej Biskupek <bartlomiej.biskupek@gmail.com>
2016-02-24 17:43:35 +0000

    didn't realise it is no longer working - fixed :)

--- dilbert/spiders/DilbertSpider.py
+++ dilbert/spiders/DilbertSpider.py
@@ -7,19 +7,18 @@ from urlparse import urljoin
 
 class DilbertSpider(BaseSpider):
     name = 'dilbert'
-    start_urls = ['http://dilbert.com/']
+    start_urls = ['http://dilbert.com/strip/2010-01-01']
 
     def __init__(self, name=None, **kwargs):
         super(DilbertSpider, self).__init__(name, **kwargs)
-        self.link_extractor = SgmlLinkExtractor(allow='/strips/comic/20(10|11|12|13)-[0-9-]{5}/$')
+        self.link_extractor = SgmlLinkExtractor(allow='/strip/20(10|11|12|13)-[0-9-]{5}$')
 
     def parse(self, response):
         for link in self.link_extractor.extract_links(response):
             yield Request(link.url, self.parse)
-        strip_path = HtmlXPathSelector(response).select('//div[@class="STR_Image"]/img/@src').extract()
+        strip_path = HtmlXPathSelector(response).select('//meta[@property="og:image"]/@content').extract()
         if strip_path:
-            strip_path = urljoin(response.url, strip_path[0])
             strip = DilbertStrip()
-            strip['image'] = strip_path
-            strip['name'] = '%s.%s' % (response.url.split('/')[-2], strip_path.split('.')[-1])
+            strip['image'] = strip_path.pop()
+            strip['name'] = '%s.gif' % response.url.split('/')[-1]
             yield strip
